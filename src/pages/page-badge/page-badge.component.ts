1afa2811d17741de3eecfef65956b8d32e844888
Arron McCrory <arronmccrory@gmail.com>
2024-09-16 16:40:46 -0400

    button page wip

--- src/pages/page-badge/page-badge.component.ts
+++ src/pages/page-badge/page-badge.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BadgeComponent } from "../../../ui/badge/badge.component";
 
 @Component({
   selector: 'demo-page-badge',
   standalone: true,
-  imports: [],
+  imports: [BadgeComponent],
   templateUrl: './page-badge.component.html',
   styleUrl: './page-badge.component.scss'
 })
